3ac0673b5739f683539eaecc1ecfdc10f08c3270
BrendonH <80381428+BrendonH18@users.noreply.github.com>
2021-07-14 23:26:05 -0400

    Added Total
    I decided to push myself a bit and see how I could do this in a simple way.

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 document.getElementById('submit').addEventListener('click', addExpense);
 
+let formatter = new Intl.NumberFormat('us-US', {
+  style: 'currency',
+  currency: 'USD',
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
+});
+
 function addExpense(event) {
   event.preventDefault();
 
@@ -11,19 +18,15 @@ function addExpense(event) {
   const expenseAmount = expenseRow.insertCell(3);
   const expenseCategory = expenseRow.insertCell(4);
   const expenseDelete = expenseRow.insertCell(5);
-  
-  let formatter = new Intl.NumberFormat('us-US', {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2,
-  });
 
   const expenseDeleteButton = document.createElement('button');
   const image = new Image(20,20);
   image.src = "delete.jpg";
   expenseDeleteButton.appendChild(image);
-  expenseDeleteButton.addEventListener('click', () => expenseRow.remove());
+  expenseDeleteButton.addEventListener('click', () => {
+    expenseRow.remove();
+    renderTotal();
+  });
 
   expenseDate.textContent = getFormatedDate(document.getElementById('expenseDate').value);
   expenseDescription.textContent = document.getElementById('expenseDescription').value;
@@ -31,9 +34,21 @@ function addExpense(event) {
   expenseAmount.textContent = formatter.format(document.getElementById('expenseAmount').value);
   expenseCategory.textContent = document.getElementById('expenseCategory').value;
   expenseDelete.appendChild(expenseDeleteButton);
-
+  
+  renderTotal();
+  
   document.getElementById('form').reset();
 }
+
+function renderTotal() {
+  const rows = document.getElementById('expenseTable').rows.length
+  let list = 0;
+  for (let i = rows - 1; i > 0; i--) {
+    list += parseFloat(document.getElementById('expenseTable').rows[i].cells[3].innerText.substring(1))
+  }
+  document.getElementById("total").textContent = formatter.format(list)
+}
+renderTotal()
 
 function getFormatedDate(date) {
   [year, month, day] = date.split("-");
@@ -41,3 +56,4 @@ function getFormatedDate(date) {
     return "";
   } else return `${month}/${day}/${year}`;
 }
+
